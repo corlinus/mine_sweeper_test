13264b130db8d580f923b195e55f6ef350568f35
Yuriy Esaulov <corlinus@gmail.com>
2016-06-30 01:36:39 +0300

    Solution

--- lib/board_map.rb
+++ lib/board_map.rb
@@ -1,4 +1,46 @@
 class BoardMap
-  BOMB_CHAR  = '*'
-  EMPTY_CHAR = '.'
+  BOMB_CHAR  = '*'.freeze
+  EMPTY_CHAR = '.'.freeze
+
+  attr_accessor :size, :mines
+
+  def initialize(size, mines)
+    @size = size
+    @mines = mines
+  end
+
+  def to_s
+    board_map.map(&:join).join("\n") + "\n"
+  end
+
+  private
+
+  def board_map
+    return @board_map if @board_map
+    @board_map = empty_board
+    mines.each { |x, y| add_mine @board_map, x, y }
+    @board_map
+  end
+
+  def empty_board
+    Array.new(size).fill { Array.new(size).fill { EMPTY_CHAR.dup } }
+  end
+
+  def add_mine(board_map, x, y)
+    [x - 1, x, x + 1].product [y - 1, y, y + 1] do |tx, ty|
+      case
+      when tx < 0             then next
+      when tx > size - 1      then next
+      when ty < 0             then next
+      when ty > size - 1      then next
+      when tx == x && ty == y then board_map[ty][tx] = BOMB_CHAR.dup
+      else
+        case board_map[ty][tx]
+        when EMPTY_CHAR then board_map[ty][tx] = 1
+        when Fixnum     then board_map[ty][tx] += 1
+        else next
+        end
+      end
+    end
+  end
 end
--- lib/mine_sweeper.rb
+++ lib/mine_sweeper.rb
@@ -1,3 +1,17 @@
+require 'yaml'
+
 class MineSweeper
-  BOARDS_FILE = 'boards.yml'
+  BOARDS_FILE = 'boards.yml'.freeze
+
+  attr_accessor :level_data
+
+  def initialize(level = nil)
+    raise ArgumentError.new 'No level given' if level.nil?
+    @level_data = YAML.load_file(BOARDS_FILE).find { |x| x['type'] == level }.freeze
+    raise ArgumentError.new 'wrong level given' if level_data.nil?
+  end
+
+  def render(buffer)
+    buffer << BoardMap.new(level_data['size'], level_data['mines']).to_s
+  end
 end
